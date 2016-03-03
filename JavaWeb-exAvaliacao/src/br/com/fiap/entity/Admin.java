83590d6269c839f9884ea378b92d0fc17b0d880a
Eduardo Araujo <vycanismajoriscsa@gmail.com>
2016-03-03 14:39:45 -0300

    Added @Entity no Admin

--- JavaWeb-exAvaliacao/src/br/com/fiap/entity/Admin.java
+++ JavaWeb-exAvaliacao/src/br/com/fiap/entity/Admin.java
@@ -1,13 +1,22 @@
 package br.com.fiap.entity;
 
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 
+@Entity
+@PrimaryKeyJoinColumn(name = "rm"  )
 public class Admin extends Usuario {
 	
-	
-
 	public Admin() {
 		this.setTipo(TipoUsuarioEnum.ADMIN);
 	}
 
+	@Override
+	public String toString() {
+		return String.format("%s[id=%d]", getClass().getSimpleName(), getId());
+	}
+
+	
+
+	
 }
